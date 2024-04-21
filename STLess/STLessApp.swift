050e80dd990e4d7d4f7f75a4962a6689861b2bc2
iosdevSW <iosdev.sw@gmail.com>
2024-04-21 18:44:04 +0900

    Initial Commit

---
+++ STLess/STLessApp.swift
@@ -0,0 +1,17 @@
+//
+//  STLessApp.swift
+//  STLess
+//
+//  Created by iOS신상우 on 4/21/24.
+//
+
+import SwiftUI
+
+@main
+struct STLessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
