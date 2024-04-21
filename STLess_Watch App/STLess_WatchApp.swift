654210502f33c3d94b1febcba3a29574a14dfaa1
iosdevSW <iosdev.sw@gmail.com>
2024-04-21 19:06:17 +0900

    Chore: 프로젝트 생성

---
+++ STLess_Watch App/STLess_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  STLess_WatchApp.swift
+//  STLess_Watch Watch App
+//
+//  Created by iOS신상우 on 4/21/24.
+//
+
+import SwiftUI
+
+@main
+struct STLess_Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
